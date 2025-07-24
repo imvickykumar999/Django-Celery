c59b019ef84ab589642fdc2ac2e92be17b98bf39
Vicky Kumar <imvickykumar999@gmail.com>
2025-07-24 16:30:25 +0530

    Update admin.py

--- newsletter/main/admin.py
+++ newsletter/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TelegramMessage
 
-# Register your models here.
+class TelegramMessageAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'message_id', 'last_message')
+    search_fields = ('chat_id', 'message_id')
+    list_filter = ('chat_id',)
+    list_editable = ('last_message',)
+    
+    fieldsets = (
+        (None, {
+            'fields': ('chat_id', 'message_id', 'last_message')
+        }),
+    )
+
+admin.site.register(TelegramMessage, TelegramMessageAdmin)
